a82c03810a492e2997d423ad6f955f0fece77ffb
Nick Markwell <markwellna@upmc.edu>
2013-11-14 12:12:40 -0500

    Make it actually work.

--- lib/chronic/bang.rb
+++ lib/chronic/bang.rb
@@ -1,7 +1,10 @@
 require "chronic/bang/version"
+require "chronic"
 
 module Chronic
+  class ParseError < StandardError; end
+
   def self.parse!(text, options = {})
-    parse(text, options) || raise RuntimeError, "could not parse date: #{text}"
+    parse(text, options) || (raise ParseError, "could not parse date: #{text}")
   end
 end
