3504196622793f421dafa75708a0fdb7576505d7
Nick Markwell <markwellna@upmc.edu>
2013-11-14 12:45:03 -0500

    Well that was kindof silly.

--- lib/chronic/bang.rb
+++ lib/chronic/bang.rb
@@ -5,6 +5,6 @@ module Chronic
   class ParseError < StandardError; end
 
   def self.parse!(text, options = {})
-    parse(text, options) || (raise ParseError, "could not parse date: #{text}")
+    parse(text, options) || raise(ParseError, "could not parse date: #{text}")
   end
 end
